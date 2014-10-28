856c8beb72ca7d527551b4a1f649b7947fbbd5e9
NAKAJIMA, Ryuichi (a.k.a. libero18) <orebil.18@gmail.com>
2014-10-28 17:57:33 +0900

    update

--- Casks/MuteCon.rb
+++ Casks/MuteCon.rb
@@ -1,7 +1,10 @@
-class MuteCon < Cask
+class Mutecon < Cask
+  version '2.02'
+  sha256 '557719f674270523c3ba75f3f57e51987cf132037a7e7b6a780b63e4f0657ab6'
+
   url 'http://www.sara-mac.com/fw/mutec202.dmg'
   homepage 'http://homepage1.nifty.com/macbs/'
-  version '2.02'
-  sha1 '668c97f5488e19afdffd4fdf4100c9f7343175cd'
-  link 'MuteCon.app'
+  license :unknown
+
+  app 'MuteCon.app'
 end
---
+++ brew-mycask.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class BrewMycask < Formula
+  homepage 'https://github.com/libero18/homebrew-mycask/'
+  url 'https://github.com/libero18/homebrew-mycask.git'
+  def install
+  end
+end
